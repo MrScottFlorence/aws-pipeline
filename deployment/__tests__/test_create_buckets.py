39578cd7fbebc0a56a8d8075b8dee8a4f776940f
scottflorence <mrscottflo@gmail.com>
2022-12-13 12:18:52 +0000

    Error handling for starting the connection to aws

--- deployment/__tests__/test_create_buckets.py
+++ deployment/__tests__/test_create_buckets.py
@@ -2,6 +2,7 @@ from deployment.src.create_buckets import Create_resources
 from moto import mock_s3
 import pytest
 from unittest.mock import patch
+from botocore.exceptions import ClientError
 
 
 @patch('deployment.src.create_buckets.os')
@@ -13,4 +14,26 @@ def test_create_resource_creates_connection_on_instance_creation(os):
         'AWS_SECRET_KEY':"tempSecret"}}
     creator = Create_resources()
     assert creator.s3 != None
-    
+
+@mock_s3
+def test_create_resource_states_the_error_when_a_the_correct_github_secrets_have_not_been_made():
+    """Testing the response when an access key is missing using mocked os response"""
+    creator = Create_resources()
+    result = creator.errors[0]
+    assert result == "Failed to find keys 'AWS_ACCESS_KEY' and 'AWS_SECRET_KEY' on key 'GITHUB_TOKEN'"
+
+@patch('deployment.src.create_buckets.boto3')
+@patch('deployment.src.create_buckets.os')
+def test_create_resource_states_when_a_user_error_such_as_invalid_password_has_been_made(os,boto):
+    """Testing the response when an client error is raised on client creation by intercepting boto's client creation"""
+    os.environ.return_value = {'GITHUB_TOKEN':{
+        'AWS_ACCESS_KEY':"temp",
+        'AWS_SECRET_KEY':"tempSecret"}}
+    class patched_boto:
+        def client(*,region_name,aws_access_key_id,aws_secret_access_key):
+            raise ClientError(error_response={'Error':{'Message':'Client error content','Code':'123'}},operation_name="op")
+    boto.return_value = patched_boto
+    creator = Create_resources()
+    result = creator.errors[0]
+    assert result == ""
+ 
--- deployment/src/create_buckets.py
+++ deployment/src/create_buckets.py
@@ -5,18 +5,41 @@ from botocore.config import Config
 
 
 class Create_resources():
+    errors = []
     def __init__(self):
         """Initialise the create resources class with the s3 connection started"""
         self.create_aws_connection()
 
     def create_aws_connection(self):
         """Create the s3 client, using secrets obtained from github secrets"""
-        github_secrets: dict = os.environ['GITHUB_TOKEN']
         try:
+            print(os.environ['GITHUB_TOKEN']['AWS_ACCESS_KEY'])
+            github_secrets: dict = os.environ['GITHUB_TOKEN']
+            print("Secrets : ",github_secrets)
             self.s3 = boto3.client('s3',
                                    region_name='us-east-1',
                                    aws_access_key_id=github_secrets['AWS_ACCESS_KEY'],
                                    aws_secret_access_key=github_secrets['AWS_SECRET_KEY'])
         except ClientError as ce:
-            print(ce.response)
+            print(ce['Error'])
+            error = 'Client Error :' + ce.response['Error']['Message']
+            print(error)
+            self.errors.append(error)
+            raise ce
+        except AttributeError as ae:
+            print(ae)
+            error = "Failed to find attributes 'AWS_ACCESS_KEY' and 'AWS_SECRET_KEY' on key 'GITHUB_TOKEN'"
+            print(error)
+            self.errors.append(error)
+        except KeyError as ke:
+            print("Error = " +str(ke))
+            error = "Failed to find keys 'AWS_ACCESS_KEY' and 'AWS_SECRET_KEY' on key 'GITHUB_TOKEN'"
+            print(error)
+            self.errors.append(error)
+        except Exception as e:
+            print(e)
+            self.errors.append(e)
+            
+    
+            
             
