7a8f5918f2fbd6533d6abfd61a1cbfeca53773c5
scottflorence <mrscottflo@gmail.com>
2022-12-14 16:33:42 +0000

    Script that when run, creates the s3 buckets, and is ready to recieve lambda paths

--- deployment/run_bucket_creation.py
+++ deployment/run_bucket_creation.py
@@ -1,1 +1,33 @@
-print("Run bucket")
+from src.create_buckets import Create_resources
+
+
+def create_buckets():
+    ingest_lambda_path = ""
+    process_payments_lambda_path = ""
+    process_purchases_lambda_path = ""
+    process_sales_lambda_path = ""
+    warehouse_uploader_lambda_path = ""
+    code_bucket_name = 'code-bucket'
+    processed_bucket_name = 'processed-bucket'
+    ingest_bucket_name = 'ingest-bucket'
+    create = Create_resources()
+    create.create_s3_bucket('code-bucket')
+    create.create_s3_bucket('ingest-bucket')
+    create.create_s3_bucket('processed-bucket')
+    if ingest_lambda_path != "":
+        create.upload_lambda_function_code(code_bucket=code_bucket_name,folder_path=ingest_lambda_path,lambda_name='ingest')
+    if process_payments_lambda_path != "":
+        create.upload_lambda_function_code(code_bucket=code_bucket_name,folder_path=process_payments_lambda_path,lambda_name='process_payments')
+    if process_purchases_lambda_path != "":
+        create.upload_lambda_function_code(code_bucket=code_bucket_name,folder_path=process_purchases_lambda_path,lambda_name='process_purchases')
+    if process_sales_lambda_path != "":
+        create.upload_lambda_function_code(code_bucket=code_bucket_name,folder_path=process_sales_lambda_path,lambda_name='process_sales')
+    if warehouse_uploader_lambda_path != "":
+        create.upload_lambda_function_code(code_bucket=code_bucket_name,folder_path=warehouse_uploader_lambda_path,lambda_name='upload')
+    
+
+
+
+
+if __name__ == '__main__':
+    create_buckets()
