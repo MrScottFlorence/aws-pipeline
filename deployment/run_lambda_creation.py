8c21482627807b5c16b86991aa3c020341adc332
scottflorence <mrscottflo@gmail.com>
2022-12-15 16:39:34 +0000

    Create schedule event and assign it to ingest, and add prints to dictate the stage

--- deployment/run_lambda_creation.py
+++ deployment/run_lambda_creation.py
@@ -1,6 +1,7 @@
 from deployment.src.deploy_lambdas import Deploy_lambdas
 from deployment.src.assign_iam import Assign_iam
 from deployment.src.create_buckets import Create_resources
+from deployment.src.event_handler import Create_events
 
 ingest_lambda_name = "ingest"
 process_payments_lambda_name = "process_payments"
@@ -21,19 +22,27 @@ code_bucket_name = 'code-bucket'
 
 def deploy_lambdas():
     permit = Assign_iam()
-    create_roles(permit)
+    print("Creating policies")
     create_policies(permit)
+    print("Creating roles and attaching policies")
+    create_roles(permit)
     deploy = Deploy_lambdas()
+    print("Creating ingest lambda")
     create_lambdas(permit, deploy, ingest_lambda_name, ingest_role)
+    print("Creating process payments lambda")
     create_lambdas(permit, deploy, process_payments_lambda_name,
                    process_payments_role)
+    print("Creating process purchases lambda")
     create_lambdas(permit, deploy, process_purchases_lambda_name,
                    process_purchases_role)
+    print("Creating process sales lambda")
     create_lambdas(permit, deploy, process_sales_lambda_name,
                    process_sales_role)
+    print("Creating warehouse uploader lambda")
     create_lambdas(permit, deploy, upload_lambda_name, warehouse_uploader_role)
     create = Create_resources()
 
+    print("Assigning triggers to ingest bucket")
     create.assign_bucket_update_event_triggers(
         bucket_name=ingest_bucket_name, lambda_arn=deploy.lambda_arns[process_payments_lambda_name], bucket_folders=['TableName/'])
     create.assign_bucket_update_event_triggers(
@@ -42,6 +51,12 @@ def deploy_lambdas():
         bucket_name=ingest_bucket_name, lambda_arn=deploy.lambda_arns[process_sales_lambda_name], bucket_folders=['TableName/'])
     create.assign_bucket_update_event_triggers(
         bucket_name=processed_bucket_name, lambda_arn=deploy.lambda_arns[upload_lambda_name], bucket_folders=[''])
+    print("Creating scheduled trigger")
+    event = Create_events()
+    event.create_schedule_event(f'schedule-event-{ingest_lambda_name}', '5')
+    lambda_arn = deploy.lambda_arns[ingest_lambda_name]
+    event.assign_event_target(
+        schedule_name='schedule-event-{ingest_lambda_name}', target_arn=lambda_arn)
 
 
 def create_lambdas(permit: Assign_iam, deploy: Deploy_lambdas, lambda_name, role_name):
@@ -50,7 +65,6 @@ def create_lambdas(permit: Assign_iam, deploy: Deploy_lambdas, lambda_name, role
 
 
 def create_roles(permit: Assign_iam):
-
     permit.create_lambda_role(role_name=ingest_role)
     permit.attach_custom_policy(
         role_name=ingest_role, policy=f"s3-read-{ingest_bucket_name}-{ingest_lambda_name}")
