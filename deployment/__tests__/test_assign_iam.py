f7af87b1679d971f327799bf0a6f8a7eec2fd42d
scottflorence <mrscottflo@gmail.com>
2022-12-15 12:19:08 +0000

    Adjust tests to account for changed naming format

--- deployment/__tests__/test_assign_iam.py
+++ deployment/__tests__/test_assign_iam.py
@@ -68,7 +68,8 @@ def test_create_cloudwatch_logging_policy_creates_logging_policy_for_passed_lamb
 def test_create_s3_ingest_read_policy_creates_a_policy_for_passed_lambda_to_read_the_passed_bucket():
     permissions = Assign_iam()
     result = permissions.create_s3_read_write_policy("test-lambda","test-bucket")
-    assert result['Policy']['PolicyName'] == 's3-read-bucket-test-lambda'
+    print(result)
+    assert result['Policy']['PolicyName'] == 's3-read-test-bucket-test-lambda'
     assert 'Arn' in result['Policy']
     assert result['ResponseMetadata']['HTTPStatusCode'] == 200
 
@@ -77,8 +78,8 @@ def test_attach_custom_policy_adds_the_policy_to_the_appropriate_role():
     permissions = Assign_iam()
     result = permissions.create_s3_read_write_policy("test-lambda","test-bucket")
     permissions.create_lambda_role(role_name='test-role')
-    result = permissions.attach_custom_policy(role_name='test-role',policy='s3-read-bucket-test-lambda')
+    result = permissions.attach_custom_policy(role_name='test-role',policy='s3-read-test-bucket-test-lambda')
     print(result)
     assert result['ResponseMetadata']['HTTPStatusCode'] == 200
     result = permissions.iam.list_attached_role_policies(RoleName='test-role')
-    assert 's3-read-bucket-test-lambda' in [policy['PolicyName'] for policy in result['AttachedPolicies']]
+    assert 's3-read-test-bucket-test-lambda' in [policy['PolicyName'] for policy in result['AttachedPolicies']]
