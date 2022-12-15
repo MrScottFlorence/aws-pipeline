128b59e95a787afb368e5227de7f03f22f006bec
scottflorence <mrscottflo@gmail.com>
2022-12-15 10:02:55 +0000

    Added creation of lambdas using appropriate roles and zip files

--- deployment/run_lambda_creation.py
+++ deployment/run_lambda_creation.py
@@ -7,25 +7,32 @@ process_purchases_lambda_name = "process_purchases"
 process_sales_lambda_name = "process_sales"
 upload_lambda_name = "upload"
 
+ingest_role = "ingest-role"
+process_payments_role = "process-payments-role"
+process_purchases_role = "process-purchases-role"
+process_sales_role = "process-sales-role"
+warehouse_uploader_role = "warehouse-uploader-role"
+
+processed_bucket_name = 'processed-bucket'
+ingest_bucket_name = 'ingest-bucket'
+code_bucket_name = 'code-bucket'
 
 def deploy_lambdas():
     permit = Assign_iam()
     create_roles(permit)
-    create_policies()
-    create_lambdas()
+    create_policies(permit)
+    deploy = Deploy_lambdas()
+    create_lambdas(permit,deploy,ingest_lambda_name,ingest_role)
+    create_lambdas(permit,deploy,process_payments_lambda_name,process_payments_role)
+    create_lambdas(permit,deploy,process_purchases_lambda_name,process_purchases_role)
+    create_lambdas(permit,deploy,process_sales_lambda_name,process_sales_role)
+    create_lambdas(permit,deploy,upload_lambda_name,warehouse_uploader_role)
     
 
-def create_lambdas()
-    pass
+def create_lambdas(permit:Assign_iam,deploy:Deploy_lambdas,lambda_name,role_name):
+    deploy.create_lambda(lambda_name=lambda_name, code_bucket=code_bucket_name, role_arn=permit.role_arns[role_name],zip_file=f'{lambda_name}.zip')
 
-def create_roles(permit:Assign_iam):
-    ingest_role = "ingest-role"
-    process_payments_role = "process-payments-role"
-    process_purchases_role = "process-purchases-role"
-    process_sales_role = "process-sales-role"
-    warehouse_uploader_role = "warehouse-uploader-role"
-    processed_bucket_name = 'processed-bucket'
-    ingest_bucket_name = 'ingest-bucket'
+def create_roles(permit:Assign_iam):    
     
     permit.create_lambda_role(role_name=ingest_role)
     permit.attach_custom_policy(role_name=ingest_role, policy=f"s3-read-{ingest_bucket_name}-{ingest_lambda_name}")
